ad50a85fc87696ec9dd8ec5c61051d7ad8cddcaa
elena971914 <elena971914@gmail.com>
2023-11-02 09:39:35 +0200

    first project

---
+++ manicure-salon-app/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+import Appointment from "./components/Appointment";
+import Services from "./components/Services";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Customers from "./components/Customers";
+import Footer from "./components/Footer";
+import Loader from "./components/Loader";
+import Pricing from "./components/Pricing";
+
+function App() {
+  return (
+    <html>
+    <body className="main-layout">
+
+      {/* <Loader /> */}
+
+      <div className="full_bg">
+        <Header />
+        <Slider />
+      </div>
+
+      <Appointment />
+      <Services />
+      <Pricing />
+      <Blog />
+      <About />
+      <Customers />
+      <Footer />
+
+    </body>
+    </html>
+  );
+}
+
+export default App;

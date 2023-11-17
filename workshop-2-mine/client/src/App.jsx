d7c8f29d9274ebebbc830aa345c4f5bd6d663a75
elena971914 <elena971914@gmail.com>
2023-11-17 21:54:55 +0200

    'workshop-2'

---
+++ workshop-2-mine/client/src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route} from 'react-router-dom'
+import Header from './components/header/Header';
+import Catalogue from './components/catalogue/Catalogue';
+import CreatePage from './components/create-page/CreatePage';
+import DetailsPage from './components/details-page/DetailsPage';
+import EditPage from './components/edit-page/EditPage';
+import Home from './components/home/Home';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/games" element={<Catalogue />} />
+        <Route path="/games/create" element={<CreatePage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ workshop-2-mine/client/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+
+export default function Header() {
+    return(<header>
+        <h1><Link className="home" to="#">GamesPlay</Link></h1>
+        <nav>
+            <Link to="#">All games</Link>
+            <div id="user">
+                <Link to="#">Create Game</Link>
+                <Link to="#">Logout</Link>
+            </div>
+            <div id="guest">
+                <Link to="#">Login</Link>
+                <Link to="#">Register</Link>
+            </div>
+        </nav>
+    </header>)
+}
